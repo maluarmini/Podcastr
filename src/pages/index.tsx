ea2ec38e4626f86e9631d627eace3cb3cc84ede4
maluarmini <luiza.armini@gmail.com>
2021-04-23 14:15:07 -0300

    Criamos um build do projeto - trabalhando em SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
-export default function Home() {
+import { useEffect } from "react"
+
+export default function Home(props) {
+  /* estratégia usando SPA
+  
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data  => console.log(data))
+  }, []);
+  */
+  console.log(props.episodes);
+
+  
   return (
     <h1>Home</h1>
   )
+}
+
+
+export async function getStaticProps() {
+ const response = await fetch('http://localhost:3333/episodes');
+ const data =  await response.json();
+
+ return {
+   props: {
+     episodes: data,
+   },
+   revalidate: 60 * 60 * 8, // determina de quanto em quanto tempo serão feita chamas api - 8 hrs 
+ }
 }
